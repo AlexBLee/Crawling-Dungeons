d4266e407fa4de8ee0a51743b33e72f2bf0c3836
Alex Lee <alexanderbevislee@gmail.com>
2020-01-09 00:41:23 -0800

    Organized UI scripts to folders

--- Assets/Scripts/Spells/SpellFunctions.cs
+++ Assets/Scripts/Spells/SpellFunctions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// These functions are only called by the animation of each spell.
 public class SpellFunctions : MonoBehaviour
 {
     public void DestroySelf()
