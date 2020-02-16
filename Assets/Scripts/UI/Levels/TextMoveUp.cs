ea8712c96e3a83619c0a6f143b053e1a415a62f3
Alex Lee <alexanderbevislee@gmail.com>
2020-02-15 17:45:09 -0800

    Removed magic numbers in TextMoveUp script

--- Assets/Scripts/UI/Levels/TextMoveUp.cs
+++ Assets/Scripts/UI/Levels/TextMoveUp.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class TextMoveUp : MonoBehaviour
 {
+    private const float moveSpeed = 3.0f;
+    private const int destroyTime = 2;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, transform.position + new Vector3(0,1,0), 3.0f * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.up, moveSpeed * Time.deltaTime);
         StartCoroutine(DestroySelf());
     }
 
     IEnumerator DestroySelf()
     {
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(destroyTime);
         Destroy(gameObject);
         
     }
