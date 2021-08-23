5bb61f14f9bc0c5a531b35399c28d395ea0f866c
AlexBLee <alexanderbevislee@gmail.com>
2021-08-23 00:44:42 -0700

    Deleted useless bool in consumableitem

--- Assets/Scripts/Items/Consumables/ConsumableItem.cs
+++ Assets/Scripts/Items/Consumables/ConsumableItem.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class ConsumableItem : Item
 {    
-    // for the potion list to tell if already in a button
-    [HideInInspector] public bool marked = false;
     public float percentageAdd = 0.0f;
     
     public virtual void ConsumeEffect(CharacterEntity character) {}
